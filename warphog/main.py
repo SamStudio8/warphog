604f891a76ad29ab0d801a0c20501795cb0e56e4
Sam Nicholls <samstudio8@gmail.com>
2021-05-08 16:44:21 +0000

    tidy main a little

--- warphog/main.py
+++ warphog/main.py
@@ -24,51 +24,72 @@ def cli():
     warphog(args)
 
 
-def warphog(args):
+class WarpHog(object):
+    def __init__(self, n):
+        self.num_seqs = n
+        self.triangle_size = ceil(( n * (n + 1) ) / 2)
+        self.idx_map, self.idy_map = np.asarray(np.triu_indices(n), dtype=np.uint16)
 
+    def output_tsv(self, d):
+        for i in range(self.triangle_size):
+            sys.stdout.write('\t'.join([str(x) for x in [
+                self.idx_map[i],
+                self.idy_map[i],
+                d[i],
+            ]]) + '\n')
+
+def init_concrete(alphabet, encoder, loader, loader_limit, fasta):
+    if alphabet:
+        raise NotImplementedError()
     alphabet = DEFAULT_ALPHABET
+    base_converter = ENCODERS[encoder](alphabet=alphabet)
+    fa_loader = LOADERS[loader](n=loader_limit, fasta=fasta, bc=base_converter)
+    return alphabet, fa_loader
 
-    # Read sequences into block
-    base_converter = ENCODERS[args.encoder](alphabet=alphabet)
+def warphog(args):
+    # Init concrete implementations of components
+    alphabet, fa_loader = init_concrete(None, args.encoder, args.loader, args.loader_limit, args.fasta)
 
-    fa_loader = LOADERS[args.loader](n=args.loader_limit, fasta=args.fasta, bc=base_converter)
+    # Load a block of sequences from FASTA
     seq_block = fa_loader.get_block()
     l = fa_loader.get_length()
-    print("huge block loaded")
+    num_seqs = fa_loader.get_count()
+    print("huge block loaded: (%d, %d)" % (num_seqs, l))
 
+    hog = WarpHog(n=num_seqs)
+
+    # Send data block to GPU
     msa_char_block = np.frombuffer("".join(seq_block).encode(), dtype=np.byte)
     msa_gpu = gpuarray.to_gpu(msa_char_block)
 
-    num_seqs = fa_loader.get_count()
-    print(num_seqs, l)
-
-    d = np.zeros(ceil( ((num_seqs * (num_seqs+1)) / 2 )), dtype=np.uint16)
+    # Init return array and send to GPU
+    triangle_size = ceil(( num_seqs * (num_seqs + 1) ) / 2)
+    d = np.zeros(triangle_size, dtype=np.uint16)
     #d_gpu = cuda.mem_alloc(d.nbytes)
     #cuda.memcpy_htod(d_gpu, d)
     d_gpu = gpuarray.to_gpu(d)
     print("huge boi on gpu")
 
+    # Generate triangle map and send to GPU
+    idx_map_gpu = gpuarray.to_gpu(hog.idx_map)
+    idy_map_gpu = gpuarray.to_gpu(hog.idy_map)
+
+    # Init GPU grid
     THREADS_PER_BLOCK_X = 4
     THREADS_PER_BLOCK_Y = 4
     PAIRS_PER_THREAD = 1 # TODO busted lel
 
     block=(THREADS_PER_BLOCK_X, THREADS_PER_BLOCK_Y, 1)
-    #grid=( ceil(num_seqs / (PAIRS_PER_THREAD * THREADS_PER_BLOCK)), ceil(num_seqs / (THREADS_PER_BLOCK)) )
-    triangle_size = ( num_seqs * (num_seqs + 1) ) / 2
     grid_width = sqrt(triangle_size)
     print( (grid_width / (PAIRS_PER_THREAD * THREADS_PER_BLOCK_X)) * (grid_width / (THREADS_PER_BLOCK_Y)) * THREADS_PER_BLOCK_X * THREADS_PER_BLOCK_Y)
     grid=( ceil(grid_width / (PAIRS_PER_THREAD * THREADS_PER_BLOCK_X)), ceil(grid_width / (THREADS_PER_BLOCK_Y)) )
     print(block)
     print(grid)
 
-    idx_map, idy_map = np.asarray(np.triu_indices(num_seqs), dtype=np.uint16)
-    idx_map_gpu = gpuarray.to_gpu(idx_map)
-    idy_map_gpu = gpuarray.to_gpu(idy_map)
-
     print("THREAD COUNT %d" % (block[0]*block[1]*grid[0]*grid[1]))
-    print("MAPS LEN", len(idx_map), len(idy_map))
+    print("MAPS LEN", len(hog.idx_map), len(hog.idy_map))
 
-
+    # Hog the warps
     start = datetime.datetime.now()
 
     kernel = KERNELS[args.kernel]()
@@ -81,7 +102,7 @@ def warphog(args):
         np.int32(l), # msa stride
         d_gpu.gpudata,
         np.int32(PAIRS_PER_THREAD),
-        np.uint32(len(idx_map)),
+        np.uint32(hog.triangle_size),
         idx_map_gpu,
         idy_map_gpu,
         block=block,
@@ -97,17 +118,13 @@ def warphog(args):
 
     print(d)
     dd = np.zeros((num_seqs, num_seqs), dtype=np.uint16)
-    dd[idx_map, idy_map] = d
+    dd[hog.idx_map, hog.idy_map] = d
     print(dd)
     s = (d > 0).sum()
 
     print("%d non-zero edit distances found (%.2f%%)" % (s, s/triangle_size*100.0))
-
-
     print("%.2fM sequence comparions / s" % ( (triangle_size / delta.total_seconds())/ 1e6) )
-
     total_bases = fa_loader.get_length() * triangle_size
     print("%.2fB base comparions / s" % ( (total_bases / delta.total_seconds()) / 1e9 ))
 
-
-
+    hog.output_tsv(d)
