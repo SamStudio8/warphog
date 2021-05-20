741bdedf8e1cc1ec8ddc73a447d5fd054b856eda
Sam Nicholls <samstudio8@gmail.com>
2021-05-20 16:17:28 +0000

    Encode strings and pass as char* to cdef for more speed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,9 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## 0.3.1 - 2021-05-20
+## 0.3.2 - 2021-05-20
 ### Changed
-* `--core prewarp` is approximately 16x faster than yesterday as the kernel is now precompiled with Cython, but is still a bit rubbish
+* `--core prewarp` is approximately 20x faster than yesterday as the kernel is now precompiled with Cython, but is still a bit rubbish
 
 ## 0.3.0 - 2021-05-19
 ### Added
--- warphog/kernels/hamming.pyx
+++ warphog/kernels/hamming.pyx
@@ -6,12 +6,12 @@ ctypedef int8_t DTYPE_t
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef unsigned short hamming(str seq_a, str seq_b, Py_ssize_t l, DTYPE_t[::1] ord_l, DTYPE_t[:, ::1] alphabet_mat):
+cdef unsigned short hamming(char* seq_a, char* seq_b, Py_ssize_t l, DTYPE_t[::1] ord_l, DTYPE_t[:, ::1] alphabet_mat):
     cdef unsigned short distance = 0
     cdef unsigned short a, b
     for i in range(l):
-        a = ord_l[ord(seq_a[i])]
-        b = ord_l[ord(seq_b[i])]
+        a = ord_l[seq_a[i]]
+        b = ord_l[seq_b[i]]
         distance += alphabet_mat[a][b]
     return distance
 
@@ -21,5 +21,5 @@ def kernel(data_block, num_seqs, stride_len, result_arr, num_thread_pairs, num_p
     for i in range(num_pairs):
         curr_idx = idx_map[i]
         curr_idy = idy_map[i]
-        result_arr[i] = hamming(data_block[curr_idx], data_block[curr_idy], stride_len, ord_l, equivalence_d)
+        result_arr[i] = hamming(data_block[curr_idx].encode(), data_block[curr_idy].encode(), stride_len, ord_l, equivalence_d)
     return result_arr
