4dac72173f7d9e95cd5e619c955cc0fa26797a84
Sam Nicholls <samstudio8@gmail.com>
2021-05-20 17:12:28 +0000

    Specify thread map types to cython cdef

--- warphog/kernels/hamming.pyx
+++ warphog/kernels/hamming.pyx
@@ -1,12 +1,14 @@
 cimport cython
 import numpy as np
-from libc.stdint cimport int8_t
+from libc.stdint cimport int8_t, uint16_t, uint32_t
 
 ctypedef int8_t DTYPE_t
+ctypedef uint16_t RTYPE_t
+ctypedef uint32_t MTYPE_t
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef unsigned short hamming(char* seq_a, char* seq_b, Py_ssize_t l, DTYPE_t[::1] ord_l, DTYPE_t[:, ::1] alphabet_mat):
+cdef unsigned short hamming(char* seq_a, char* seq_b, unsigned int l, DTYPE_t[::1] ord_l, DTYPE_t[:, ::1] alphabet_mat):
     cdef unsigned short distance = 0
     cdef unsigned short a, b
     for i in range(l):
@@ -17,9 +19,10 @@ cdef unsigned short hamming(char* seq_a, char* seq_b, Py_ssize_t l, DTYPE_t[::1]
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def kernel(data_block, num_seqs, stride_len, result_arr, num_thread_pairs, num_pairs, idx_map, idy_map, ord_l, equivalence_d):
+def kernel(data_block, num_seqs, unsigned int stride_len, RTYPE_t[::1] result_arr, num_thread_pairs, unsigned long num_pairs, MTYPE_t[::1] idx_map, MTYPE_t[::1] idy_map, ord_l, alphabet_mat):
+    cdef unsigned int curr_idx, curr_idy
     for i in range(num_pairs):
         curr_idx = idx_map[i]
         curr_idy = idy_map[i]
-        result_arr[i] = hamming(data_block[curr_idx].encode(), data_block[curr_idy].encode(), stride_len, ord_l, equivalence_d)
+        result_arr[i] = hamming(data_block[curr_idx].encode(), data_block[curr_idy].encode(), stride_len, ord_l, alphabet_mat)
     return result_arr
